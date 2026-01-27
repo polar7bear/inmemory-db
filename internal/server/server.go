ac41c7a814c35f620496adad431628d58e41f3ac
polar7bear <sons1998@naver.com>
2026-01-27 22:15:51 +0900

    feat: TCP 서버 구현

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,56 @@
 package server
+
+import (
+	"log"
+	"net"
+)
+
+// TCP 서버
+type Server struct {
+	listener net.Listener
+	addr     string
+}
+
+func New(addr string) *Server {
+	return &Server{addr: addr}
+}
+
+// Start는 서버를 시작하고 연결을 수신합니다.
+// 이 함수는 블로킹됩니다 (무한 루프).
+func (s *Server) Start() error {
+	listener, err := net.Listen("tcp", s.addr)
+
+	if err != nil {
+		return err
+	}
+
+	// 서버 리스너에 저장
+	s.listener = listener
+	defer s.listener.Close() // 서버 종료 전 리소스 정리
+	log.Printf("현재 서버가 [%s] 에서 리스닝중입니다.", s.addr)
+
+	// 무한루프
+	for {
+		// Accept() 호출 -> 연결대기(블로킹)
+		conn, err := s.listener.Accept()
+
+		if err != nil {
+			log.Print("연결 중 오류: ", err)
+			continue
+		} else {
+			s.handleConnection(conn)
+		}
+	}
+
+}
+
+// 단일 클라이언트 연결을 처리합니다.
+func (s *Server) handleConnection(conn net.Conn) {
+	// 연결 종료 예약
+	defer conn.Close()
+
+	// 응답 전송
+	conn.Write([]byte("+PONG\r\n"))
+
+	log.Printf("클라이언트가 성공적으로 연결되었습니다: %s", conn.RemoteAddr())
+}
