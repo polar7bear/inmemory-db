94a95516b22919e3b0c252a2216c1b75e1275f5d
polar7bear <sons1998@naver.com>
2026-02-09 17:10:51 +0900

    feat: 저장소에 동시접근하여 발생하는 레이스컨디션을 막기위해 RWMutex를 사용

--- internal/storage/store.go
+++ internal/storage/store.go
@@ -1,8 +1,10 @@
 package storage
 
+import "sync"
 
 type Store struct {
 	data map[string]string
+	mu   sync.RWMutex
 }
 
 func New() *Store {
@@ -10,10 +12,18 @@ func New() *Store {
 }
 
 func (s *Store) Set(key, value string) {
+	s.mu.Lock()
 	s.data[key] = value
+	s.mu.Unlock()
 }
 
 func (s *Store) Get(key string) (string, bool) {
+	// 서로다른 고루틴 간의 읽기 작업에서는 블로킹 없이 동시에 통과
+	// A 고루틴이 쓰기 작업 도중, B 고루틴이 데이터를 읽고 있다면 데이터 불일치 현상이 생길 수 있기때문에
+	// RLock(읽기)이 걸려있으면 Lock(쓰기)은 대기, Lock이 걸려있으면 RLock은 대기
+	s.mu.RLock()
 	value, exist := s.data[key]
+	s.mu.RUnlock()
+
 	return value, exist
 }
