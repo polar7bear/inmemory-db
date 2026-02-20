3c505e27a008dc062a83ed6eb01e92564825aa28
polar7bear <sons1998@naver.com>
2026-02-20 23:39:03 +0900

    feat: 바이너리 포맷 정의

---
+++ internal/persistence/format.go
@@ -0,0 +1,15 @@
+package persistence
+
+var MagicBytes = [6]byte{'M', 'I', 'N', 'I', 'D', 'B'}
+
+const (
+	Version byte = 0x01
+
+	TypeString byte = 0x00
+	TypeList   byte = 0x01
+
+	NoExpiry  byte = 0x00
+	HasExpiry byte = 0x01
+
+	EOF byte = 0xFF
+)
